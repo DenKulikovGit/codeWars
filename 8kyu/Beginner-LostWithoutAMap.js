65a07f2fc66de0c77df24ea3f516538ff2cc78ee
DenKulikovGit <deniskulikov95787@gmail.com>
2023-12-14 11:42:03 +0300

    MaxHeadroomAndJavaScriptStyle.js

---
+++ 8kyu/Beginner-LostWithoutAMap.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+//     Given an array of integers, return a new array with each value doubled.
+//
+//     For example:
+//
+//     [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+    let newX = x.map(x => x *2);
+    return newX;
+}
