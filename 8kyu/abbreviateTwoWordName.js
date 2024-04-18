0864b34e1a4a12f8a06a56f0c6649ba64c8bb03a
DenKulikovGit <deniskulikov95787@gmail.com>
2024-04-18 14:50:20 +0300

    abbreviateTwoWordName.js

---
+++ 8kyu/abbreviateTwoWordName.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+//     The output should be two capital letters with a dot separating them.
+//SOLUTION:
+
+function abbrevName(name) {
+    const nameParts = name.split(' ');
+    const initials = nameParts.map(part => part[0].toUpperCase());
+    return initials.join('.');
+}
+
+console.log(abbrevName("Evan Cole"))
+console.log(abbrevName("David Mendieta"))
