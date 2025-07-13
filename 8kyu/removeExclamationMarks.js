d6f6897a3dd8cc2a3dfcfbbc92453d6f623fc9af
DenKulikov <deniskulikov95787@gmail.com>
2025-07-13 11:36:52 +0300

    Remove exclamation marks

---
+++ 8kyu/removeExclamationMarks.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+//     SOLUTION:
+
+function removeExclamationMarks(s) {
+    return s.replaceAll('!', '');
+}
+
+console.log(removeExclamationMarks('Hello! World!'));
+console.log(removeExclamationMarks('!!!!JS!!!!'));
